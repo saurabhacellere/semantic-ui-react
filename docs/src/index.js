298440358df226e7911427498219d9d7404501d6
saurabh.patil@acellere.com <saurabh.patil@acellere.com>
2019-10-17 17:32:35 +0530

    commitMessage : test(docs): add visual regression testing

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
 
 import App from './App'
 
 export default App
 
+if (process.env.FAKER_SEED) {
+  faker.seed(parseInt(process.env.FAKER_SEED, 10))
+}
+
 if (typeof document !== 'undefined') {
   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
-  const render = (Component) => renderMethod(<Component />, document.getElementById('root'))
+  const render = Component => renderMethod(<Component />, document.getElementById('root'))
 
   render(App)
 }
